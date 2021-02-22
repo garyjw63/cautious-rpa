44b824e3b1a6d6a6c4a9e0f1518c702d555cadae
Me <gary_withers@hotmail.com>
2021-02-22 18:34:20 +0000

    First commit

---
+++ Hello World/Res1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\Administrator\\Documents\\Unified Functional Testing\\cautious-rpa\\Hello World\\Res1',namePairs:[]}]};
